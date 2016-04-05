265f4f8ce4c8f27255736cc64354a5d0aae7d28e
Michael Kukar <mkukar@gmail.com>
2016-04-05 12:10:16 -0700

    Added html/php code that exists on the raspberry pi

---
+++ RPi/html/index.php
@@ -0,0 +1,5 @@
+<?php echo "EE459 Final Project Group 15<br>"; ?>
+<?php echo "IP ADDR: ", exec('hostname -I'), "<br>"; ?>
+<?php echo "Group Members:<br>"; ?>
+<?php echo "Kukar, Ellis, Alvarado, Yu<br>"; ?>
+<input type="button" value="Test Button" onclick="location='buttonPress.php'"/>
---
+++ RPi/html_readme.txt
@@ -0,0 +1,1 @@
+Copy into /var/www/html directory of raspberry pi
